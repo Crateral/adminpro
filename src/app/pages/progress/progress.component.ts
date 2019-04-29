bca64ff0c496adb77310b0f1514abc125087d78b
Cesar Hidalgo <proveedor_chidalgo@ath.com.co>
2019-04-29 15:56:41 -0500

    Cambios 29/04

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,15 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
-  porcentaje: number = 50;
+  porcentaje1: number = 20;
+  porcentaje2: number = 30;
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  cambiarValor(valor){
-    this.porcentaje = this.porcentaje + valor;
   }
 
 }
